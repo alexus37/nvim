dd68c2f0a05f9866781828a2493613232c1a3ca7
Alex Lelidis <alexus37@github.com>
2022-02-16 14:21:51 +0100

    remove space binding in insert mode

--- lua/user/autopairs.lua
+++ lua/user/autopairs.lua
@@ -13,7 +13,7 @@ npairs.setup {
   },
   disable_filetype = { "TelescopePrompt", "spectre_panel" },
   fast_wrap = {
-    map = "<leader>c",
+    map = "<H>c",
     chars = { "{", "[", "(", '"', "'" },
     pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
     offset = 0, -- Offset from pattern match
